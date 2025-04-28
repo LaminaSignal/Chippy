45a9e41da4e929381bcaca67d23fadc30ad61007
Mariana Perez Garcia <perezgarciam@microsoft.com>
2025-04-27 18:54:39 -0600

    fix interface

--- dist/core/IChippy.d.ts
+++ dist/core/IChippy.d.ts
@@ -1,6 +1,6 @@
 import { ChippyConfig } from "../types";
 export interface IChippy {
     speak: (message: string) => void;
-    pulse: (tableName: string, eventType: string, payload?: any) => void;
+    pulse: (eventType: string, payload?: any) => void;
     configureChippy: (config: ChippyConfig) => void;
 }
